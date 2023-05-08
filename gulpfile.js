002da6858b903b151a154e422b161d85570a6907
Jas <jasminemacn@gmail.com>
2023-05-08 13:11:26 -0400

    gulp success

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,11 @@
 var gulp = require('gulp');
 var plumber = require('gulp-plumber');
 const uglify = require('gulp-uglify');
-const sass = require('gulp-sass');
+const sass = require('gulp-sass')(require('sass'));
 const wait = require('gulp-wait');
 const babel = require('gulp-babel');;
 const rename = require('gulp-rename');
+
 
 gulp.task('scripts', function() {
     return gulp.src('./js/scripts.js')
@@ -35,5 +36,18 @@ gulp.task('styles', function () {
 
 gulp.task('watch', function() {
     gulp.watch('./js/scripts.js', gulp.series('scripts'));
+    gulp.watch('./scss/styles.scss', gulp.series('styles'));
+});
+
+
+
+gulp.task('styles', function () {
+    return gulp.src('./scss/styles.scss')
+        .pipe(wait(250))
+        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
+
+gulp.task('watch', function() {
     gulp.watch('./scss/styles.scss', gulp.series('styles'));
 });
